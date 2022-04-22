0b42635ab341a222389db60c19824e0a0726a87a
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-04-22 23:04:26 +0300

    Добавила функции авторизации и регистрации

---
+++ Core/Authorization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Core.DateBase;
+
+namespace Core
+{
+    public class Authorization
+    {
+        public static ObservableCollection<User> users { get; set; }
+        public static DateBase.User AuthorizationUser(string login, string password)
+        {
+            users = new ObservableCollection<User>(bd_connection.connection.User.ToList());
+            var userExists = users.Where(user => user.Login == login && user.Password == password).FirstOrDefault();
+            if(userExists != null)
+            {
+                return userExists;
+            }
+            else
+            {
+                return userExists;
+            }
+ 
+        }
+    }
+}
---
+++ Core/Registration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DateBase;
+using System.Collections.ObjectModel;
+
+namespace Core
+{
+    public class Registration
+    {
+        public static ObservableCollection<User> users { get; set; }
+
+        public static void RegistrationUser(string nick, string login, string password)
+        {
+            User newUser = new User();
+            int startXP = 0;
+            int startLvl = 1;
+
+            newUser.Nickname = nick;
+            newUser.Login = login;
+            newUser.Password = password;
+            newUser.User_Level = startLvl;
+            newUser.Count_XP = startXP;
+
+            bd_connection.connection.User.Add(newUser);
+            bd_connection.connection.SaveChanges();
+        }
+    }
+}
