7bbc189a8d38f020d9d99ba4bcdda4994155377a
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-04-22 23:09:09 +0300

    Добавила функцию вывода всех колллекций определенного юзера

---
+++ Core/CollectionFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DateBase;
+using System.Collections.ObjectModel;
+
+namespace Core
+{
+    public class CollectionFunction
+    {
+        public static ObservableCollection<Collection> collections { get; set; }
+
+        public static ObservableCollection<Collection> GetCollection(int idUser)
+        {
+            return collections = new ObservableCollection<Collection>((bd_connection.connection.Collection.Where(userCollectiom => userCollectiom.ID_User == idUser)).ToList());
+        }
+    }
+}
